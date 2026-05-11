bf17ef74d9a9ad1d0ad03ffbabd213ddc453cbd7
c4m1l0-07 <juan.hernandez73@correo.tdea.edu.co>
2026-05-11 07:27:33 -0500

    Update Consultorio.java-- Se agrego metodos estaDisponible y toString

--- src/main/java/com/tdea/Consultorio.java
+++ src/main/java/com/tdea/Consultorio.java
@@ -1,31 +1,52 @@
 public class Consultorio {
+
     private int numero;
     private String tipo;
     private String estado;
 
-    public Consultorio(){
+    public Consultorio() {
     }
-    public Consultorio(int numero,String tipo,String estado){
-        this.numero=numero;
-        this.tipo=tipo;
-        this.estado=estado;
+
+    public Consultorio(int numero, String tipo, String estado) {
+        this.numero = numero;
+        this.tipo = tipo;
+        this.estado = estado;
     }
-    public int getNumero(){
+
+    public int getNumero() {
         return numero;
     }
-    public void setNumero(int numero){
-        this.numero=numero;
+
+    public void setNumero(int numero) {
+        this.numero = numero;
     }
-    public String getTipo(){
+
+    public String getTipo() {
         return tipo;
     }
-    public void setTipo(String tipo){
-        this.tipo=tipo;
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
-    public String getEstado(){
+
+    public String getEstado() {
         return estado;
     }
-    public void setEstado(String estado){
-        this.estado=estado;
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public boolean estaDisponible() {
+        return estado.equalsIgnoreCase("disponible");
+    }
+
+    @Override
+    public String toString() {
+        return "Consultorio{" +
+                "numero=" + numero +
+                ", tipo='" + tipo + '\'' +
+                ", estado='" + estado + '\'' +
+                '}';
     }
 }
