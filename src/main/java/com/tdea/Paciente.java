be65870bb8f46ba0f0526ac89d2c70b0b37378b5
c4m1l0-07 <juan.hernandez73@correo.tdea.edu.co>
2026-05-11 07:17:49 -0500

    Update Paciente.java--Se agrego un metodo toString

--- src/main/java/com/tdea/Paciente.java
+++ src/main/java/com/tdea/Paciente.java
@@ -1,64 +1,95 @@
 public class Paciente {
-    private Long documento;
+
+    private String documento;
     private String nombre;
     private int edad;
     private String genero;
-    private Long numero;
+    private String telefono;
     private String eps;
     private String sangre;
 
-    public Paciente(){
-    }
-    public Paciente(Long documento, String nombre, int edad, String genero, Long numero,String eps, String sangre){
-        this.documento=documento;
-        this.nombre=nombre;
-        this.edad=edad;
-        this.genero=genero;
-        this.numero=numero;
-        this.eps=eps;
-        this.sangre=sangre;
-    }
-    public Long getDocumento(){
-        return documento;
-    }
-    public void setDocumento(Long documento){
-        this.documento=documento;
-    }
-    public String getNombre(){
-        return nombre;
-    }
-    public void setNombre(String nombre){
-        this.nombre=nombre;
-    }
-    public int getEdad(){
-        return edad;
-    }
-    public void setEdad(int edad){
-        this.edad=edad;
-    }
-    public String getGenero(){
-        return genero;
-    }
-    public void setGenero(String genero){
-        this.genero=genero;
-    }
-    public Long getNumero(){
-        return numero;
-    }
-    public void setNumero(Long numero){
-        this.numero=numero;
-    }
-    public String getEps(){
-        return eps;
-    }
-    public void setEps(String eps){
-        this.eps=eps;
-    }
-    public String getSangre(){
-        return sangre;
-    }
-    public void setSangre(String sangre){
-        this.sangre=sangre;
+    public Paciente() {
     }
 
+    public Paciente(String documento, String nombre, int edad,
+                     String genero, String telefono,
+                     String eps, String sangre) {
+
+        this.documento = documento;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.genero = genero;
+        this.telefono = telefono;
+        this.eps = eps;
+        this.sangre = sangre;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEps() {
+        return eps;
+    }
+
+    public void setEps(String eps) {
+        this.eps = eps;
+    }
+
+    public String getSangre() {
+        return sangre;
+    }
+
+    public void setSangre(String sangre) {
+        this.sangre = sangre;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente{" +
+                "documento='" + documento + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", genero='" + genero + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", eps='" + eps + '\'' +
+                ", sangre='" + sangre + '\'' +
+                '}';
+    }
 }
